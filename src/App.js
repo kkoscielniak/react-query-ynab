f72356ce6962feadb5ef92456c4d43ccb2c5384c
Krystian Kościelniak <krystiankoscielniak@gmail.com>
2021-11-23 19:37:50 +0100

    Move the app to react-native-web and add react-navigation

--- src/App.js
+++ src/App.js
@@ -1,22 +1,22 @@
 import "./App.css";
-import {
-  QueryClient,
-  QueryClientProvider
-} from "react-query";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import MainScreen from "./screens/MainScreen";
 
-function App() {
+const App = () => {
   const queryClient = new QueryClient();
+  const Stack = createNativeStackNavigator();
 
   return (
     <QueryClientProvider client={queryClient}>
-      <div className="App">
-        <header className="App-header">
-          <MainScreen />
-        </header>
-      </div>
+      <NavigationContainer>
+        <Stack.Navigator>
+          <Stack.Screen name="Choose budget" component={MainScreen} />
+        </Stack.Navigator>
+      </NavigationContainer>
     </QueryClientProvider>
   );
-}
+};
 
 export default App;
--- src/screens/MainScreen.js
+++ src/screens/MainScreen.js
@@ -1,3 +1,4 @@
+import { View, Text } from "react-native-web";
 import { useQuery } from "react-query";
 import { getBudgets } from "../queries/budgets";
 
@@ -9,16 +10,16 @@ const MainScreen = () => {
 
   console.log(data);
 
-  // if (isSuccess) {
-  //   console.log(data);
-  // };
-
   return (
-    <>
-      {data?.budgets.map((budget) => {
-        return <p>{budget.name}</p>
-      })}
-    </>
+    <View className="App">
+      <header className="App-header">
+        {data?.budgets.map((budget) => {
+          return (
+            <Text style={{ color: "white", padding: 10 }}>{budget.name}</Text>
+          );
+        })}
+      </header>
+    </View>
   );
 };
 
