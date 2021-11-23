22b20426777db5f58d3f0a723deaafc881494524
Krystian Kościelniak <krystiankoscielniak@gmail.com>
2021-11-23 23:08:07 +0100

    Add fetching the transactions using react-query (without rendering yet)

--- src/queries/transactions.js
+++ src/queries/transactions.js
@@ -1,6 +1,8 @@
 import client from "./apiClient";
 
-export const getTransactions = ({ budgetId }) => {
-  const response = client.get(`/budgets/${budgetId}/transactions`);
-  return response.data.data;
+export const getTransactions = async ({ queryKey }) => {
+  const [_, budgetId] = queryKey; // this is taken from the array from useQuery
+  
+  const { data } = await client.get(`/budgets/${budgetId}/transactions`);
+  return data; 
 };
--- src/screens/BudgetDetailsScreen.js
+++ src/screens/BudgetDetailsScreen.js
@@ -1,7 +1,19 @@
 import { Button, Text, View } from "react-native-web";
+import { useQuery } from "react-query";
+import { getTransactions } from "../queries/transactions";
 
 const BudgetDetailsScreen = ({ route, navigation }) => {
   const { budgetId } = route.params;
+
+  const {
+    isLoading,
+    isSuccess,
+    data: responseData,
+    isError,
+    error,
+  } = useQuery([`transactions`, budgetId], getTransactions);
+
+  console.log(responseData);
 
   const handleBack = () => {
     navigation.goBack();
